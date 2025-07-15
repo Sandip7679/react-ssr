e9761ebaf7f27de70b1e1c6fa6ce69e1d237a0d4
Sandip Bera <107534834+Sandip7679@users.noreply.github.com>
2025-07-16 02:50:17 +0530

    commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -17,6 +17,7 @@ About.meta = {
   keywords: 'about, application, features',
   author: 'Your Name',
   robots: 'index, follow',
+  ogUrl: 'https://react-ssr-7bu1.onrender.com/about', // Optional, if you have a specific URL for the page
   image: 'https://react-ssr-7bu1.onrender.com/chatimage2.jpg', // Optional, if you have an image for social sharing
   // image: 'https://react-ssr-7bu1.onrender.com/about2.jpeg', // Optional, if you have an image for social sharing
   // image: `${process.env.BASE_URL}/about2.jpeg`, // Optional, if you have an image for social sharing
