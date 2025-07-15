608c793e11a494dd06c9614578d449952547d6e5
Sandip Bera <107534834+Sandip7679@users.noreply.github.com>
2025-07-16 02:40:56 +0530

    commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -17,7 +17,8 @@ About.meta = {
   keywords: 'about, application, features',
   author: 'Your Name',
   robots: 'index, follow',
-  image: 'https://react-ssr-7bu1.onrender.com/about2.jpeg', // Optional, if you have an image for social sharing
+  image: 'https://react-ssr-7bu1.onrender.com/chatimage2.jpg', // Optional, if you have an image for social sharing
+  // image: 'https://react-ssr-7bu1.onrender.com/about2.jpeg', // Optional, if you have an image for social sharing
   // image: `${process.env.BASE_URL}/about2.jpeg`, // Optional, if you have an image for social sharing
   ogType: 'website', // Open Graph type
 };
